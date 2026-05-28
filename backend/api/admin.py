32a3a86edf197f2c6924457eae5a8534db7a54e3
Himanshu Kumar <himanshu.kumar@handigital.com>
2026-05-28 16:04:32 +0530

    Register custom models in Django admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Organization, DataSource, UploadBatch, RawRecord, EmissionRecord, ReviewAction, AuditLog
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('role', 'organization')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('role', 'organization')}),
+    )
+    list_display = ('username', 'email', 'role', 'organization', 'is_staff')
+    list_filter = ('role', 'organization', 'is_staff', 'is_superuser')
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Organization)
+admin.site.register(DataSource)
+admin.site.register(UploadBatch)
+admin.site.register(RawRecord)
+admin.site.register(EmissionRecord)
+admin.site.register(ReviewAction)
+admin.site.register(AuditLog)
